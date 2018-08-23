9c2b1cb400792935ee2f319858bf35f87e5527db
shw2886 <42574726+shw2886@users.noreply.github.com>
2018-08-23 17:50:55 +0900

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    privateKey: ‘0xedcdc8c2edc2fc0d400947a6734003f0a9521839’,
+    contractAddress: ‘0x24682d769a8926029de8cf26e811aa2e16596677’,
+    abi: ‘0x24682d769a8926029de8cf26e811aa2e16596677’
+}
